c657464c36ab4709517cb631d0d7253d5271fe4d
vijayshinva <vijayshinva@gmail.com>
2019-03-19 16:06:54 +0530

    Chirper.Identity

---
+++ Chirper.Identity/Startup.cs
@@ -0,0 +1,89 @@
+﻿using IdentityModel;
+using IdentityServer4.Models;
+using IdentityServer4.Test;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Collections.Generic;
+using System.Security.Claims;
+
+namespace Chirper.Identity
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddIdentityServer()
+                .AddInMemoryClients(new List<Client>())
+                .AddInMemoryIdentityResources(new List<IdentityResource>())
+                .AddInMemoryApiResources(new List<ApiResource>())
+                .AddTestUsers(new List<TestUser>()
+                        {
+                            new TestUser {
+                                SubjectId = "5CE86359-063C-432B-AC2D-A3832232DACE",
+                                Username = "test",
+                                Password = "password",
+                                Claims = new List<Claim> {
+                                    new Claim(JwtClaimTypes.Email, "test@test.com"),
+                                    new Claim(JwtClaimTypes.Role, "admin")
+                                }
+                            },
+                            new TestUser {
+                                SubjectId = "3DE82379-01AC-482C-BC4E-A3238252AACE",
+                                Username = "userone",
+                                Password = "password",
+                                Claims = new List<Claim> {
+                                    new Claim(JwtClaimTypes.Email, "userone@test.com"),
+                                    new Claim(JwtClaimTypes.Role, "chirper")
+                                }
+                            }
+                        }
+                )
+                .AddDeveloperSigningCredential();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+            app.UseIdentityServer();
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
